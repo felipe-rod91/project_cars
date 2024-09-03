5df1895c1df79e4f77be20978ca232bb325d50f3
Felipe Rodrigues <felipe.rs991@gmail.com>
2024-09-03 19:06:31 -0300

    Adicionado o campo foto com opção de upload e criação de pasta com todas as fotos

--- cars/models.py
+++ cars/models.py
@@ -18,6 +18,7 @@ class Car(models.Model):
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10)
     price = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
