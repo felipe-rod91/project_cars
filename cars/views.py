e5ca6d30ad5a466a2ac0aeb5dcb5c05c339561ea
Felipe Rodrigues <felipe.rs991@gmail.com>
2024-09-06 15:32:49 -0300

    Criada a pasta template com o arquivo html de teste

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def cars_view(request):
-    return HttpResponse('Meus Carros')
+    return render(request, 'cars.html')
