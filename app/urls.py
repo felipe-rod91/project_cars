4f39673b40cff2279af25d866b4367ee7720eb19
Felipe Rodrigues <felipe.rs991@gmail.com>
2024-09-05 21:48:03 -0300

    Views transferidas para o arquivo views.py

--- app/urls.py
+++ app/urls.py
@@ -18,10 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
+from cars.views import cars_view
 
-def cars_view(request):
-    return HttpResponse('Meus Carros')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
