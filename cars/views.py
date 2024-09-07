7c5fd800806d6e5c68a81f726739b9a5c538125a
Felipe Rodrigues <felipe.rs991@gmail.com>
2024-09-07 17:57:27 -0300

    Ligando o views com o banco de dados e utilizando linguagem de template no HTML

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
+    cars = Car.objects.all()
+
     return render(
         request, 
         'cars.html', 
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars}
     )
