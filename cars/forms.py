02ece113d9ddfccdb25c274a506e8dc5015bc700
Felipe Rodrigues <felipe.git@outlook.com>
2024-09-26 21:02:10 -0300

    Função de cadastro de carros pela view adicionada

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200)
@@ -9,4 +9,17 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     price = forms.DecimalField(max_digits=10, decimal_places=2)
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            price = self.cleaned_data['price'],
+            photo = self.cleaned_data['photo'],
+        )
+        car.save()
+        return car
     
