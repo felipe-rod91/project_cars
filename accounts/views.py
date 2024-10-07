8f7b2c576915bd0a51525c938daac23fa7a43b0f
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-07 16:04:03 -0300

    Criado o template e a rota da tela de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -10,3 +11,8 @@ def register_view(request):
     else:
         user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form})
+
+
+def login_view(request):
+    login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
