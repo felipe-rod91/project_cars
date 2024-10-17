a038acd672df9a6e6dd43d032ae935229d747c53
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-17 19:56:26 -0300

    Migração da função cars_view para Class Based View

--- cars/views.py
+++ cars/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('brand__name')
-    search = request.GET.get('search')
-    
-    if search:
-        cars = cars.filter(model__icontains=search)
+class CarsView(View):
 
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars}
-    )
+    def get(self, request):
+        cars = Car.objects.all().order_by('brand__name')
+        search = request.GET.get('search')
+        
+        if search:
+            cars = cars.filter(model__icontains=search)
+
+        return render(
+            request, 
+            'cars.html', 
+            {'cars': cars}
+        )
+
 
 def new_car_view(request):
     if request.method == 'POST':
