9379609e2ea0baba93d024dd349e35923fadd376
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-17 20:31:31 -0300

    Função new_car_view transformada em Class Based View

--- cars/views.py
+++ cars/views.py
@@ -29,3 +29,17 @@ def new_car_view(request):
     else:
         new_car_form = CarModelForm()
     return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+
+class NewCarView(View):
+    
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+    
+    def post(self, request):
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
