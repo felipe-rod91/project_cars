550998f73c566638d7005d8f78c3a8eb722f25e2
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-17 22:15:15 -0300

    Alterando a classe CarsView para CarsListView

--- cars/views.py
+++ cars/views.py
@@ -2,33 +2,20 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
-class CarsView(View):
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    def get(self, request):
-        cars = Car.objects.all().order_by('brand__name')
-        search = request.GET.get('search')
-        
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('brand')
+        search = self.request.GET.get('search')
         if search:
             cars = cars.filter(model__icontains=search)
-
-        return render(
-            request, 
-            'cars.html', 
-            {'cars': cars}
-        )
-
-
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return cars
 
 
 class NewCarView(View):
