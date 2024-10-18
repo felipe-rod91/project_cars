7b62d41dac7c9b6b28b543a641b04b84e16629a4
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-17 22:57:46 -0300

    Alterando a classe NewCarView para NewCarCreateView

--- cars/forms.py
+++ cars/forms.py
@@ -5,10 +5,3 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
-
-
-    def clean_price(self):
-        price = self.cleaned_data.get('price')
-        if price < 20000:
-            self.add_error('price', 'Preço do carro não pode ser menor do que R$20.000,00.')
-        return price
--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 
 class CarsListView(ListView):
@@ -11,7 +11,7 @@ class CarsListView(ListView):
     context_object_name = 'cars'
 
     def get_queryset(self):
-        cars = super().get_queryset().order_by('brand')
+        cars = super().get_queryset().order_by('brand__name')
         search = self.request.GET.get('search')
         if search:
             cars = cars.filter(model__icontains=search)
@@ -30,3 +30,11 @@ class NewCarView(View):
             new_car_form.save()
             return redirect('cars_list')
         return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+    
