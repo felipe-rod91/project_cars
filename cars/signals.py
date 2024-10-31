6b55e5af6045f9bf7e5b86ba3317958b3c62ec0a
Felipe Rodrigues <felipe.git@outlook.com>
2024-10-30 23:52:13 -0300

    Criado o arquivo de dependências do projeto

--- cars/signals.py
+++ cars/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Sum
 from cars.models import Car, CarInventory
-from cars.gemini_integration import get_car_AI_bio
+from gemini_api.car_bio_generator import get_car_AI_bio
 
 
 @receiver(pre_save, sender=Car)
